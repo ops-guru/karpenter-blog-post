9ee38d90ac8bb04773fa2f300eec1dd08e3d692d
Fernando Battistella <fernando@opsguru.io>
2021-12-21 09:41:50 -0700

    initial code
    Signed-off-by: Fernando Battistella <fernando@opsguru.io>

---
+++ terraform/main.tf
@@ -0,0 +1,101 @@
+# code based on Karpenter's getting started guide at https://karpenter.sh/docs/getting-started-with-terraform/
+# - added outputs
+# - changed vpc to two AZs/subnets
+# - removed helm release in favor of cli helm
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.cluster_name
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${var.region}a", "${var.region}b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+  }
+}
+
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+
+  cluster_version = "1.21"
+  cluster_name    = var.cluster_name
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.private_subnets
+  enable_irsa     = true
+
+  worker_groups = [
+    {
+      instance_type = "m5.large"
+      asg_max_size  = 1
+    }
+  ]
+}
+
+data "aws_iam_policy" "ssm_managed_instance" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "karpenter_ssm_policy" {
+  role       = module.eks.worker_iam_role_name
+  policy_arn = data.aws_iam_policy.ssm_managed_instance.arn
+}
+
+resource "aws_iam_instance_profile" "karpenter" {
+  name = "KarpenterNodeInstanceProfile-${var.cluster_name}"
+  role = module.eks.worker_iam_role_name
+}
+
+module "iam_assumable_role_karpenter" {
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                       = "4.7.0"
+  create_role                   = true
+  role_name                     = "karpenter-controller-${var.cluster_name}"
+  provider_url                  = module.eks.cluster_oidc_issuer_url
+  oidc_fully_qualified_subjects = ["system:serviceaccount:karpenter:karpenter"]
+}
+
+resource "aws_iam_role_policy" "karpenter_controller" {
+  name = "karpenter-policy-${var.cluster_name}"
+  role = module.iam_assumable_role_karpenter.iam_role_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:CreateLaunchTemplate",
+          "ec2:CreateFleet",
+          "ec2:RunInstances",
+          "ec2:CreateTags",
+          "iam:PassRole",
+          "ec2:TerminateInstances",
+          "ec2:DescribeLaunchTemplates",
+          "ec2:DescribeInstances",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DescribeInstanceTypeOfferings",
+          "ec2:DescribeAvailabilityZones",
+          "ssm:GetParameter"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+output "iam_role_arn" {
+  value = module.iam_assumable_role_karpenter.iam_role_arn
+}
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "cluster_name" {
+  description = "The name of the cluster"
+  type        = string
+}
+
+variable "region" {
+  description = "The name of the AWS region to deploy to"
+  type        = string
+}
